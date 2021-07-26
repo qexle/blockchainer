5faf5bc559f9e343bde10795e807e50f8b47d19c
rainmaker6 <masteroftheirclassic@gmail.com>
2021-07-26 11:39:11 +0530

    Added CI, removed comments, refactored code

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,112 +3,110 @@ extern crate serde;
 extern crate serde_json;
 extern crate sha2;
 
-use sha2::{Sha256, Digest};
+use self::sha2::{Sha256, Digest};
 use std::fmt::Write;
 
 #[derive(Debug, Clone, Serialize)]
-struct Transcation{
+struct Transaction {
     sender: String,
-    receiver: String,
+    receiver: String,  
     amount: f32,
 }
 
-//Header struct for our block.
 #[derive(Serialize, Debug)]
-pub struct Blockheader{
+pub struct Blockheader {
     timestamp: i64,
-    nonce: u32,
-    pre_hash: String.
-    merkle: String,
-    difficutly: u32,
+    nonce: u32, 
+    pre_hash: String,  
+    merkle: String,  
+    difficulty: u32,
 }
 
 #[derive(Serialize, Debug)]
 pub struct Block {
     header: Blockheader,
-    count: u32,//amount of transactions in the block.
-    transactions: Vec<Transcation>,
+    count: u32,
+    transactions: Vec<Transaction>
 }
 
 pub struct Chain {
     chain: Vec<Block>,
-    curr_trans: Vec<Transcation>,
-    difficutly: u32,
-    miner_addr: String,
+    curr_trans: Vec<Transaction>,
+    difficulty: u32,
+    miner_addr: String, 
     reward: f32,
 }
 
 impl Chain {
-    pub fn new(miner_addr: String, difficutly: u32) -> Chain {
-        //We'll create a chain by binding it to a mutable value chain.
+    pub fn new(miner_addr: String, difficulty: u32) -> Chain {
         let mut chain = Chain {
             chain: Vec::new(),
             curr_trans: Vec::new(),
-            difficutly,
+            difficulty,
             miner_addr,
-            reward: 100.0,//when our chain starts we have initially 100 coins.
-            //and then we can decide that how we want to distribute the coins.
+            reward: 100.0,
         };
+
         chain.generate_new_block();
-        chain//return our chain from this function.
+        chain
+
     }
-    pub fn generate_new_block(&mut self, sender: String, reciever: String, amount: f32) -> bool {
-        //generate a new block with the given values.
-        self.curr_trans.push(Transcation{
+
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
+        self.curr_trans.push(Transaction{
             sender,
-            reciever, 
+            receiver,
             amount,
         });
+
         true
     }
-    pub fn last_hash(&self) -> String{
-        //self if the sturct we are implementing means Chain.
-        let block = match self.chain.last(){
-            //we'll bind the last block of the chain to block.
-            //if we get a block we output it otherwise we output 
-            //a vector with 48 zeros.
-            None => return String::from_utf8(vec![48; 64]).unwrap()
-            //this is accounting for our genesis block.
-            //when we create our chain the last hash won't exist.
-            //so we need to fill it out with someting so we reuturn zeros.
 
+    pub fn last_hash(&self) -> String {
+        let block = match self.chain.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48; 64]).unwrap()
         };
         Chain::hash(&block.header)
-        pub fn update_difficulty(&mut self, difficutly: u32) -> bool {{
-            self.difficutly = difficutly;
-            true
-        }
-        pub fn update_reward(&mut self, reward: f32) -> bool {
-            self.reward = reward;
-            true
-        }
-        pub fn generate_new_block(&mut self) -> bool {
-            let header = Blockheader{
-                timestamp: time::now().to_timespec().sec,
-                nonce: 0,
-                merkle: String::new(),
-                pre_hash: self.last_hash(),
-                difficutly: self.difficutly,
-            };
+    }
 
-            let reward_trans = Transcation{
-                sender: String::from("Root"),
-                reciever: self.miner_addr.clone(),
-                amount: self.reward
-            };
+    pub fn update_difficulty(&mut self, difficulty: u32) -> bool {
+        self.difficulty = difficulty;
+        true
+    }
 
-            let mut block = Block {
-                header,
-                count: 0,
-                transactions: vec![]
-            };
-            block.transactions.push(reward_trans);
-            block.transactions.append(&mut self.curr_trans);
-            block.count = block.transactions.len() as u32;
-            block.header.merkle = Chain::get_merkle(block.transactions.clone());
-            //function will calculate merkle hash accordingly.
+    pub fn update_reward(&mut self, reward: f32) -> bool {
+        self.reward = reward;
+        true
+    }
+
+    pub fn generate_new_block(&mut self) -> bool {
+        let header = Blockheader {
+            timestamp: time::now().to_timespec().sec,
+            nonce: 0,
+            pre_hash: self.last_hash(),
+            merkle: String::new(),
+            difficulty: self.difficulty
+        };
+
+        let reward_trans = Transaction {
+            sender: String::from("Root"),
+            receiver: self.miner_addr.clone(),
+            amount: self.reward
+        };
+
+        let mut block = Block {
+            header,
+            count: 0,
+            transactions: vec![]
+        };
+
+        block.transactions.push(reward_trans);
+        block.transactions.append(&mut self.curr_trans);
+        block.count = block.transactions.len() as u32;
+        block.header.merkle = Chain::get_merkle(block.transactions.clone());
         Chain::proof_of_work(&mut block.header);
-        //function will calculate the proof of work.
+
         println!("{:#?}", &block);
         self.chain.push(block);
         true
@@ -128,23 +126,17 @@ impl Chain {
         }
 
         while merkle.len() > 1 {
-            //This will iterate untill we've
-            //a singly length inside of our merkle vector.
             let mut h1 = merkle.remove(0);
             let mut h2 = merkle.remove(0);
             h1.push_str(&mut h2);
             let nh = Chain::hash(&h1);
             merkle.push(nh);
         }
-        //when we finally have a single lenght we pop it off 
-        //and return it.
         merkle.pop().unwrap()
     }
-    //final hash that comes from combining all the other hashes
-    //inside of the vector will be the hash that will be our
-    //quote unquote merkle hash.
+
     pub fn proof_of_work(header: &mut Blockheader) {
-        loop {//as difficulty goes up we need to loop more and mpre
+        loop {
             let hash = Chain::hash(header);
             let slice = &hash[..header.difficulty as usize];
             match slice.parse::<u32>() {
@@ -163,12 +155,6 @@ impl Chain {
             };
         }
     }
-    //This function will loop around untill we have 
-    //the proper nonce it gives us a hash with a certain
-    //number of leading zeros.
-    //difficulty: 2
-    //then
-    //hash: 00832478347583492504950
 
     pub fn hash<T: serde::Serialize>(item: &T) -> String {
         let input = serde_json::to_string(&item).unwrap();
@@ -189,4 +175,3 @@ impl Chain {
     }
 
 }
-    }
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,25 @@
-//Blockchain is like a database and we can store any kind of data on it.
-//Transcation records are most often stored.
-//Every blockchain has previos hash, timestamp, TX_ID(Transcation root) which are 
-//filled with bunch of transcation and hashed and a Nonce.
-//Transcation root also called a merkle root.
-
-//When a new transcation comes in it gets stored on a block and then.
-//and the block enters the chain after somebody starts mining the bock.
-//when people start mining the block and the block is shown up on the blockchain.
-// How mining works is so we get the previous hash and we start throwing random bits
-//into an algorithm based on the nounce we have untill we get a pattern.
-//If the pattern matches and it is corresponds to the nounce so we mine the block and 
-//the block gets produced on the blockchain.
-//difficutly corresponds to the number of leading zeros in the hash.
-//most block chains are distributed which means that we can have multiple
-//different blockchains that have the same data on multiple different computers(nodes).
-//if the block chain changes on a single computer then other chains on every computer
-//will tell that it was changed and they will update their data.
-// time lib so we'll be able to get time stamps
-// serde allows us to serialize and deserialize data.
-// so it allows to basically put our blockchain into a JSON format.
-// sha2 allows us to create sha2 hashes for our blockchain.
-#[macro_use]//This will allow us to derive all the serializes 
-//from blockchain.rs on our main.rs
-
+#[macro_use]
 extern crate serde_derive;
 
 use std::io;
 use std::process;
 use std::io::Write;
 
-mod blockchain;//import blockchain.rs header file
+mod blockchain;
 
 fn main() {
     let mut miner_addr = String::new();
     let mut difficulty = String::new();
     let mut choice = String::new();
 
-    print!("Enter a miner address: ");
+    print!("Enter miner address: ");
     io::stdout().flush();
     io::stdin().read_line(&mut miner_addr);
     print!("Difficulty: ");
     io::stdout().flush();
     io::stdin().read_line(&mut difficulty);
     let diff = difficulty.trim().parse::<u32>().expect("Enter an integer");
-    println!("Generating genesis block !...");
+    println!("Generating genesis block ! ...");
     let mut chain = blockchain::Chain::new(miner_addr.trim().to_string(), diff);
 
     loop {
@@ -60,20 +36,20 @@ fn main() {
         println!("");
 
         match choice.trim().parse().unwrap() {
-            0 =>//cases 0, exit
+            0 =>
             {
                 println!("exiting!");
                 process::exit(0);
             },
-            1 => {//cases 1, new transaction
+            1 => {
                 let mut sender = String::new();
                 let mut receiver = String::new();
                 let mut amount = String::new();
 
-                print!("Enter sender address:");
+                print!("enter sender address:");
                 io::stdout().flush();
                 io::stdin().read_line(&mut sender);
-                print!("Enter receiver address: ");
+                print!("enter receiver address: ");
                 io::stdout().flush();
                 io::stdin().read_line(&mut receiver);
                 print!("Enter amount: ");
@@ -84,24 +60,24 @@ fn main() {
                                         receiver.trim().to_string(), 
                                         amount.trim().parse().unwrap());
 
-                match res {//What new_transaction function returns.
-                    true => println!("Transaction Sucessful"),
+                match res {
+                    true => println!("Transaction Sucessfully Added"),
                     false => println!("Transaction Failed"),
                 }
             },
-            2 =>//cases 2, mine block
+            2 =>
             {
                 println!("Generating block");
                 let res = chain.generate_new_block();
                 match res {
-                    true => println!("Block generated successfully"),
-                    false => println!("Block generation failed"),
+                    true => println!("Block Generated Successfully"),
+                    false => println!("Block Generation Failed"),
                 }
             },
-            3 =>//cases 3, change difficulty
+            3 =>
             {
                 let mut new_diff = String::new();
-                print!("Enter new difficulty: ");
+                print!("enter new difficulty: ");
                 io::stdout().flush();
                 io::stdin().read_line(&mut new_diff);
                 let res = chain.update_difficulty(new_diff.trim().parse().unwrap());
@@ -110,7 +86,7 @@ fn main() {
                     false => println!("Failed Update Difficulty"),
                 }
             },
-            4 =>{//cases 4, change reward
+            4 =>{
                 let mut new_reward = String::new();
                 print!("Enter new reward: ");
                 io::stdout().flush();
@@ -122,7 +98,7 @@ fn main() {
                 }
             }
             _ => println!("Invalid option please retry"),
-            //input other then 0, 1, 2, 3, 4
         }
+
     }
 }
